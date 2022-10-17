c2a51224fdd9c45118a7c31f596d428ad9f27afe
ipeters123 <isaiah.peters64@gmail.com>
2022-10-17 19:00:14 +0000

    Updated Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (var word in pronoun) {
+  for (var wordTwo in adj) {
+    for (var wordThree in noun) {
+      console.log(pronoun[word] + adj[wordTwo] + noun[wordThree] + ".com");
+    }
+  }
+}
